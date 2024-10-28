7e209596a7705196ca5daf6156ec6d1814cd7ec6
BlazerXXI <ruslandendik@gmail.com>
2024-10-28 23:12:46 +0200

    add Analytics vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import HomePage from "./pages/HomePage";
-import About from "./pages/AboutPage";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Work from "./pages/Work";
-import Contact from "./pages/Contact";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import HomePage from './pages/HomePage'
+import { Analytics } from '@vercel/analytics/react'
+
+import About from './pages/AboutPage'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Work from './pages/Work'
+import Contact from './pages/Contact'
 
 function App() {
 	return (
@@ -13,16 +15,17 @@ function App() {
 			<Header />
 			<main>
 				<Routes>
-					<Route path="/" element={<HomePage />} />
-					<Route path="/about" element={<About />} />
-					<Route path="/work" element={<Work />} />
-					<Route path="/contact" element={<Contact />} />
+					<Route path='/' element={<HomePage />} />
+					<Route path='/about' element={<About />} />
+					<Route path='/work' element={<Work />} />
+					<Route path='/contact' element={<Contact />} />
 				</Routes>
 			</main>
 			<Footer />
 			<ToastContainer />
+			<Analytics />
 		</BrowserRouter>
-	);
+	)
 }
 
-export default App;
+export default App
