fcf3cd0465e1c81dc68b8a021982c5c742dd632f
BlazerXXI <ruslandendik@gmail.com>
2024-10-30 13:19:20 +0200

    fix images, fix about section

--- src/pages/HomePage/About.tsx
+++ src/pages/HomePage/About.tsx
@@ -1,18 +1,18 @@
-import { Link } from "react-router-dom";
-import LinkIcon from "../../components/icons/LinkIcon";
+// import { Link } from "react-router-dom";
+// import LinkIcon from "../../components/icons/LinkIcon";
 
 const About = () => {
 	return (
-		<section className="about flex justify-between items-center max-lg:flex-col max-lg:gap-6">
-			<div className="max-lg:w-full lg:w-[173px] xl:w-[310px]">
-				<p className="page-title lg:text-end">About me</p>
+		<section className='about flex justify-between max-lg:flex-col max-lg:gap-6'>
+			<div className='max-lg:w-full lg:w-[173px] xl:w-[310px]'>
+				<p className='page-title lg:text-end'>About me</p>
 			</div>
-			<div className="lg:max-w-[720px] flex flex-col gap-[45px]">
-				<div className="flex flex-col gap-4">
-					<h2 className="page-subtitle max-lg:hidden">
+			<div className='lg:max-w-[720px] flex flex-col gap-[45px]'>
+				<div className='flex flex-col gap-4'>
+					<h2 className='page-subtitle max-lg:hidden'>
 						I am a Front-end developer.
 					</h2>
-					<p className="page-paragraph">
+					<p className='page-paragraph'>
 						As a motivated and skilled Front-End Developer, I bring extensive
 						experience in crafting dynamic web applications and ecommerce
 						platforms using powerful tools like React. Renowned for my
@@ -21,15 +21,15 @@ const About = () => {
 						innovative projects within the tech industry.
 					</p>
 				</div>
-				<Link
+				{/* <Link
 					to="/about"
 					className="primary-link btn-hover items-center gap-1 inline-flex w-fit max-lg:w-full"
 				>
 					More about me <LinkIcon primary />
-				</Link>
+				</Link> */}
 			</div>
 		</section>
-	);
-};
+	)
+}
 
-export default About;
+export default About
